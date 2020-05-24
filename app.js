00a27f09a951aeb8463c1e63b0838620c62c0716
Eduardo Simiele Araujo <contatoedu@outlook.com>
2020-05-24 16:47:03 -0300

    teste #2

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/openshift', function (req, res) {
+  res.send('Hello Openshift!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
