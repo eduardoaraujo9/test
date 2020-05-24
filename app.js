fc2e6f0437d969d5dd492500eac55bcecbdb9f3d
Eduardo Simiele Araujo <contatoedu@outlook.com>
2020-05-24 17:05:18 -0300

    looper

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/openshift', function (req, res) {
   res.send('Hello Openshift!\n');
 });
 
+app.get('/looper', function (req, res) {
+  res.send('<html><body><h1>' + os.hostname() + '</h1><script>setTimeout(function(){window.location.href=window.location.origin+"/looper?rand="+Math.round(Math.random()*10000)},1000);</script></body></html>');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
